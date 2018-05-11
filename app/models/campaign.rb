a6a15cc18ded0dc8ce352fec92251b2455467ea1
Student <example@example.com>
2018-05-11 14:22:00 -0700

    add views for campaigns model

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,6 +1,7 @@
 class Campaign < ApplicationRecord
   has_many :reviews
 
+  validates :name, :presence => true
   validates :type_name, :presence => true
   validates :specialization, :presence => true
 end
--- db/migrate/20180511205537_create_campaigns.rb
+++ db/migrate/20180511205537_create_campaigns.rb
@@ -1,6 +1,7 @@
 class CreateCampaigns < ActiveRecord::Migration[5.2]
   def change
     create_table :campaigns do |t|
+      t.column :name, :string
       t.column :type_name, :string
       t.column :user_id, :integer
       t.column :employees, :string
