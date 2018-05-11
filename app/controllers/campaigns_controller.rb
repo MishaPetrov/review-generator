180bab87931ad104a508640ab50200af20130f46
Student <example@example.com>
2018-05-11 15:33:00 -0700

    add views and controller methods to create new reviews and campaigns

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -8,4 +8,23 @@ class CampaignsController < ApplicationController
     @campaign = Campaign.find(params[:id])
     render :show
   end
+
+  def new
+    @campaign = Campaign.new
+    render :new
+  end
+
+  def create
+    @campaign = Campaign.new(campaign_params)
+    if @campaign.save
+      redirect_tpo campaigns_path
+    else
+      render :new
+    end
+  end
+
+private
+  def campaign_params
+    params.require(:campaign).permit(:name, :type_name, :specialization, :user_id)
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,11 @@
 class ReviewsController < ApplicationController
   def index
-    @lists = List.all
+    @reviews = Reviews.all
     render :index
+  end
+
+  def new
+    @campaign = Campaign.find(params[:campaign_id])
+    @review = @campaign.reviews.new
   end
 end
