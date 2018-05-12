2dbd6f9694cd11237b807f89efc952b82ae4769e
Student <example@example.com>
2018-05-11 19:47:34 -0700

    successfully generate first sentence

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,7 +9,7 @@ class ReviewsController < ApplicationController
     @campaign = Campaign.find(params[:campaign_id])
     @review = @campaign.reviews.new(review_params)
     length = review_params["length"].to_i
-    @review.content = generate_service_review(length)
+    @review.content = @review.generate_service_review(length)
     if @review.save
       redirect_to campaign_path(@review.campaign)
     else
@@ -17,29 +17,19 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @campaign = Campaign.find(params[:campaign_id])
+    @review = @campaign.reviews.find(params[:id])
+    @review.destroy
+    redirect_to campaign_path(@campaign)
+  end
+
 private
-
-  personal_pronouns = ["I", "you", "he", "she", "it", "we", "you", "they"]
-  possessive_pronouns = ["mine", "yours", "his", "hers", "its", "ours", "yours", "theirs"]
-  time = ["just", "recently", "few days ago", "yestersay", "last week", "last month"]
-
-  # Moving and cleaning services specific
-  place_size = ["1br", "1 bedroom", "one bedroom", "2br", "2 bedroom", "two bedroom", "3br", "3 bedroom", "three bedroom", "4br", "4 bedroom", "four bedroom"]
-  place_type = ["apartment", "condo", "house", "duplex", "single home"]
-
-  # Moving specific
-  intro_sentences = ["(reviewer) just moved from a (place) to a (place) with the help of (company)"]
 
   def review_params
     params.require(:review).permit(:length)
   end
 
-  def generate_service_review(length)
-    return "Fuck you!"
-  end
 
-  def generate_product_review(length)
-
-  end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,44 @@ class Review < ApplicationRecord
   belongs_to :campaign
 
   validates :length, presence: true, :numericality => { :greater_than => 19, :less_than => 501 }
+
+  # DATA TO BUILD REVIEWS
+  PERSONAL_PRONOUNS = ["I", "we", "you", "he", "she", "it", "you", "they"]
+  POSSESSIVE_PRONOUNS = ["mine", "ours", "yours", "his", "hers", "its", "yours", "theirs"]
+  TIME = ["just", "recently", "few days ago", "yestersay", "last week", "last month"]
+
+  # Moving and cleaning services specific
+  PLACE_SIZE = ["1br", "1 bedroom", "one bedroom", "2br", "2 bedroom", "two bedroom", "3br", "3 bedroom", "three bedroom", "4br", "4 bedroom", "four bedroom"]
+  PLACE_TYPE = ["apartment", "condo", "house", "duplex", "single home"]
+
+  # Moving specific
+  INTRO_SENTENCES = ["(reviewer) just moved from a (old_place) to a (new_place) with the help of (company).", ]
+
+  def generate_service_review(length)
+    final_review = ""
+    intro = INTRO_SENTENCES.sample
+    if intro.include? '(reviewer)'
+      intro.sub! '(reviewer)', PERSONAL_PRONOUNS[rand(0..1)]
+    end
+    if intro.include? '(old_place)'
+      place = ""
+      place << PLACE_SIZE.sample << " " << PLACE_TYPE.sample
+      intro.sub! '(old_place)', place
+    end
+    if intro.include? '(new_place)'
+      place = ""
+      place << PLACE_SIZE.sample << " " << PLACE_TYPE.sample
+      intro.sub! '(new_place)', place
+    end
+    if intro.include? '(company)'
+      intro.sub! '(company)', campaign.name
+    end
+    final_review << intro
+    return final_review
+  end
+
+  def generate_product_review(length)
+
+  end
+
 end
